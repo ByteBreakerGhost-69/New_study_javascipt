d783f54af3f22962959c9e2cc416230a41373898
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-04-06 04:13:40 +0700

    Add greeting function in Belajar_junction.js

---
+++ Study_javascript/Belajar_junction.js
@@ -0,0 +1,8 @@
+// function adalah blok c9de yang bisa di panggil berkali kali supaya tidak menulis logika yang sama secara berulang
+
+function sapa(nama) {
+    return "Halo, " + nama + "!";
+}
+
+console.log(sapa("Dunia")); // Output: Halo, Dunia!
+

95087e6f69a780afb17af776069552fa7668d26b
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-04-06 04:14:46 +0700

    Add array example with hobbies in Belajar_array.js

---
+++ Study_javascript/Belajar_array.js
@@ -0,0 +1,11 @@
+let hobi = ["Coding", "Membaca", "Gaming"];
+
+// Mengakses data (dimulai dari 0)
+console.log(hobi[0]); // Hasil: "Coding"
+
+// Menambah data ke akhir list
+hobi.push("Renang");
+
+// Menghitung jumlah data
+console.log(hobi.length); // Hasil: 4
+

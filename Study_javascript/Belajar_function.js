1911cf9819068f851c69e330888eade60d054830
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-04-06 04:18:38 +0700

    Add function to sum two numbers in Belajar_function.js

---
+++ Study_javascript/Belajar_function.js
@@ -0,0 +1,10 @@
+// gunvion iti adalah kumpilan code yang kamu bungkus dalam 1 nama agar bisa di panggil.
+// structurenya:
+// function, nama fungsi, parameter/argument, return
+
+function tambah(a, b) {
+    return a + b;
+}
+
+let hasil = tambah(10, 5); // Memanggil fungsi
+console.log(hasil); // Output: 15

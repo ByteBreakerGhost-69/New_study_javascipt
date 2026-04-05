18d5465f202617931d53f5d04c891f93349c0620
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-04-06 04:12:39 +0700

    Add conditional and loop examples in JavaScript

---
+++ Study_javascript/Belajar_structure_kendali.js
@@ -0,0 +1,14 @@
+// pengondisian ( if ... else )
+// untuk memberikan intruksi agar program dapat berfikir
+let nilai = 80;
+if (nilai >= 75) {
+    console.log("Lulus!");
+} else {
+    console.log("Coba lagi.");
+}
+
+// pengulangan ( for loop )
+// untuk menjalankan code berkali kali tanpa mengulang
+for (let i = 1; i <= 5; i++) {
+    console.log("Baris ke-" + i);
+}

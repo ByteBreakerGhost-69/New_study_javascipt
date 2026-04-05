6339968e7cc98b0d89f422be8d57a97b9c8535a5
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-04-06 04:11:12 +0700

    Update and rename Belajar_JavaScript to Belajar_variable.js

---
+++ Study_javascript/Belajar_variable.js
@@ -0,0 +1,5 @@
+let skor = 10; // let untuk data yang nilainya bisa berubah 
+skor = 20; // Ini boleh
+
+const nama = "ByteBreaker"; //const untuk data yang nilainya tetap 
+// nama = "Ghost"; // Ini akan error karena const tidak bisa diubah

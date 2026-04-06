8bb76f2d5f6af025684727a329f94d1b5c100db9
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-04-07 03:59:18 +0700

    Enhance DOM manipulation examples in script
    Added examples for insertBefore, removeChild, and replaceChild methods in DOM manipulation.

--- Study_javascript/Belajar _ Node _ manipulation.js
+++ Study_javascript/Belajar _ Node _ manipulation.js
@@ -15,3 +15,49 @@ const ul = document.querySelector('ul#daftar');
 
 // Tempelkan li ke dalam ul
 ul.appendChild(liBaru);
+
+
+// 4. insertBefore()
+// jika appendChild selalu menempelkan di paling bawah, insertBefore di gunakkan 
+// jika kamu ingin menyelipkan element baru di antara elemen lainnya.
+
+
+// 5. removeChild() & replaceChild()
+// removeChild() itu untuk menghapus element dari halaman.
+// repalceChild() itu untuk menukar element lama dengan yang baru.
+
+<!-- contoh: -->
+<div id="container">
+    <ul id="log-list">
+        <li>Sistem Ready...</li>
+    </ul>
+    <button id="btn-tambah">Tambah Log Data</button>
+</div>
+
+// 1. Seleksi elemen pemicu dan kontainer
+const tombol = document.getElementById('btn-tambah');
+const ul = document.getElementById('log-list');
+
+// 2. Buat fungsi saat tombol diklik
+tombol.addEventListener('click', function() {
+    
+    // --- PROSES NODE MANIPULATION ---
+    
+    // A. Bikin elemen li baru
+    const liBaru = document.createElement('li');
+    
+    // B. Bikin isi teksnya (misal pakai waktu sekarang)
+    const waktu = new Date().toLocaleTimeString();
+    const teksLog = document.createTextNode('Data diterima pada: ' + waktu);
+    
+    // C. Gabungkan teks ke dalam li
+    liBaru.appendChild(teksLog);
+    
+    // D. Beri sedikit style agar menarik (Manipulation Style)
+    liBaru.style.color = 'green';
+    liBaru.style.fontWeight = 'bold';
+    
+    // E. Tempelkan ke dalam UL (Muncul di layar!)
+    ul.appendChild(liBaru);
+    
+});

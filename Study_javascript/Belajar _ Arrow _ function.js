5f6f934b98a7240d5ad31e50108f5bd9a3ac29a2
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-04-07 23:31:31 +0700

    Add explanation and example of arrow functions
    Menambahkan penjelasan tentang fungsi panah dan contoh kode.

---
+++ Study_javascript/Belajar _ Arrow _ function.js
@@ -0,0 +1,8 @@
+// Arrow function adalah cara menulis fungsi le ih singkat dengan menggunakkan '=>' selain lenih rapi, dia juga punya pernedaan dengan kata kunci thia.
+
+<! contoh kode:
+const mahasiswa = ['Budi', 'Ani', 'Caca'];
+
+// Versi Ringkas
+const jumlahHuruf = mahasiswa.map(nama => nama.length);
+console.log(jumlahHuruf); // [4, 3, 4]

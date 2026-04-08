1086b2609e9a55e65d2872fc1145d9182ff231f8
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-04-08 20:36:37 +0700

    Update Belajar _ structure _ kendali.js

--- Study_javascript/Belajar _ structure _ kendali.js
+++ Study_javascript/Belajar _ structure _ kendali.js
@@ -12,3 +12,19 @@ if (nilai >= 75) {
 for (let i = 1; i <= 5; i++) {
     console.log("Baris ke-" + i);
 }
+
+
+//do....while 
+//structure penulisan:
+do {
+    // 1. Jalankan kode ini dulu
+} while ( kondisi ); // 2. Baru cek, apakah harus diulang?
+
+//contoh dasar:
+let i = 1;
+
+do {
+    console.log("Angka ke-" + i);
+    i++; // Kita tambah 1 agar angkanya naik
+} while (i <= 5);
+
